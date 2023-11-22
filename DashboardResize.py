820bd77b067977e8e1fad48f0151ad18cec449eb
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-11-22 16:50:10 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -2,12 +2,14 @@ import streamlit as st
 import xml.etree.ElementTree as ET
 from io import BytesIO
 
-def calculer_nouvelles_valeurs(x, w, maxwidth, nouvelle_largeur):
+def calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur, nouvelle_hauteur):
     nouveau_x = x / (100000 / maxwidth) * (100000 / nouvelle_largeur)
     nouveau_w = w / (100000 / maxwidth) * (100000 / nouvelle_largeur)
-    return int(nouveau_x), int(nouveau_w)
+    nouveau_y = y / (100000 / maxheight) * (100000 / nouvelle_hauteur)
+    nouveau_h = h / (100000 / maxheight) * (100000 / nouvelle_hauteur)
+    return int(nouveau_x), int(nouveau_w), int(nouveau_y), int(nouveau_h)
 
-def modifier_tableau_de_bord(xml_path, nouvelle_largeur, dashboard_name):
+def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name):
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
@@ -16,14 +18,21 @@ def modifier_tableau_de_bord(xml_path, nouvelle_largeur, dashboard_name):
             maxwidth = float(size.get("maxwidth", 1.0))
             size.set("maxwidth", str(nouvelle_largeur))
             size.set("minwidth", str(nouvelle_largeur))
+            maxheight = float(size.get("maxheight", 1.0))
+            size.set("maxheight", str(nouvelle_hauteur))
+            size.set("minheight", str(nouvelle_hauteur))
 
         for zone in dashboard.findall(".//zone"):
             x = int(zone.get("x", 0))
             w = int(zone.get("w", 0))
-            nouveau_x, nouveau_w = calculer_nouvelles_valeurs(x, w, maxwidth, nouvelle_largeur)
+            y = int(zone.get("y", 0))
+            h = int(zone.get("h", 0))
+            nouveau_x, nouveau_w, nouveau_y, nouveau_h = calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur, nouvelle_hauteur)
             zone.set("x", str(nouveau_x))
             zone.set("w", str(nouveau_w))
-
+            zone.set("y", str(nouveau_y))
+            zone.set("h", str(nouveau_h))
+            
     nouveau_nom_fichier = "nouveau_fichier.twb"
     tree.write(nouveau_nom_fichier)
     return nouveau_nom_fichier
@@ -34,11 +43,12 @@ def main():
     # Sidebar
     dashboard_name = st.sidebar.text_input("Nom du Tableau de Bord", placeholder="Ex: Overview")
     nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord",placeholder="Ex:1600",min_value=1, max_value=3000, value=None, step=1)
+    nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord",placeholder="Ex:1800",min_value=1, max_value=6000, value=None, step=1)
     xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"])
 
     if xml_path:
         if st.sidebar.button("Modifier"):
-            fichier_modifie = modifier_tableau_de_bord(xml_path, nouvelle_largeur, dashboard_name)
+            fichier_modifie = modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name)
 
             # Télécharger le fichier modifié
             st.download_button(
