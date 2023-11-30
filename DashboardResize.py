b7ed5d2cc33461aa6b29696b0a28ab67cb2ee41e
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-11-30 18:08:00 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -15,10 +15,11 @@ def calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur
     return int(nouveau_x), int(nouveau_w), int(nouveau_y), int(nouveau_h)
 
 def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name):
-    with open(xml_path, 'r') as file:
-        xml_content = file.read()
-        st.text(f"Contenu du fichier XML :\n{xml_content}")
-    tree = ET.parse(xml_path)
+
+    xml_content = xml_path.read()
+    st.text(f"Contenu du fichier XML :\n{xml_content}")    
+    xml_bytes = BytesIO(xml_content)
+    tree = ET.parse(xml_bytes)
     root = tree.getroot()
 
     for dashboard in root.findall(".//dashboard[@name='"+dashboard_name+"']"):
