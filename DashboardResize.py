fd5f39a906bf02c1b818ac07bf47714796dde696
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-11-30 15:41:31 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -9,30 +9,37 @@ def calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur
     nouveau_h = h / (100000 / maxheight) * (100000 / nouvelle_hauteur)
     return int(nouveau_x), int(nouveau_w), int(nouveau_y), int(nouveau_h)
 
-def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name):
+def recuperer_noms_dashboards(xml_path):
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    return [dashboard.get("name") for dashboard in root.findall(".//dashboard")]
+
+def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboards_a_modifier):
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
-    for dashboard in root.findall(".//dashboard[@name='"+dashboard_name+"']"):
-        for size in dashboard.findall(".//size"):
-            maxwidth = float(size.get("maxwidth", 1.0))
-            size.set("maxwidth", str(nouvelle_largeur))
-            size.set("minwidth", str(nouvelle_largeur))
-            maxheight = float(size.get("maxheight", 1.0))
-            size.set("maxheight", str(nouvelle_hauteur))
-            size.set("minheight", str(nouvelle_hauteur))
+    for dashboard in root.findall(".//dashboard"):
+        dashboard_name = dashboard.get("name")
+        if dashboard_name in dashboards_a_modifier:
+            for size in dashboard.findall(".//size"):
+                maxwidth = float(size.get("maxwidth", 1.0))
+                size.set("maxwidth", str(nouvelle_largeur))
+                size.set("minwidth", str(nouvelle_largeur))
+                maxheight = float(size.get("maxheight", 1.0))
+                size.set("maxheight", str(nouvelle_hauteur))
+                size.set("minheight", str(nouvelle_hauteur))
 
-        for zone in dashboard.findall(".//zone"):
-            x = int(zone.get("x", 0))
-            w = int(zone.get("w", 0))
-            y = int(zone.get("y", 0))
-            h = int(zone.get("h", 0))
-            nouveau_x, nouveau_w, nouveau_y, nouveau_h = calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur, nouvelle_hauteur)
-            zone.set("x", str(nouveau_x))
-            zone.set("w", str(nouveau_w))
-            zone.set("y", str(nouveau_y))
-            zone.set("h", str(nouveau_h))
-            
+            for zone in dashboard.findall(".//zone"):
+                x = int(zone.get("x", 0))
+                w = int(zone.get("w", 0))
+                y = int(zone.get("y", 0))
+                h = int(zone.get("h", 0))
+                nouveau_x, nouveau_w, nouveau_y, nouveau_h = calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur, nouvelle_hauteur)
+                zone.set("x", str(nouveau_x))
+                zone.set("w", str(nouveau_w))
+                zone.set("y", str(nouveau_y))
+                zone.set("h", str(nouveau_h))
+
     nouveau_nom_fichier = "nouveau_fichier.twb"
     tree.write(nouveau_nom_fichier)
     return nouveau_nom_fichier
@@ -41,14 +48,17 @@ def main():
     st.title("Modification de Tableau de Bord")
 
     # Sidebar
-    dashboard_name = st.sidebar.text_input("Nom du Tableau de Bord", placeholder="Ex: Overview")
-    nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord",placeholder="Ex:1600",min_value=1, max_value=3000, value=None, step=1)
-    nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord",placeholder="Ex:1800",min_value=1, max_value=6000, value=None, step=1)
     xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"])
 
     if xml_path:
+        dashboards = recuperer_noms_dashboards(xml_path)
+        dashboards_a_modifier = st.sidebar.multiselect("Dashboards à modifier", dashboards)
+        nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord", placeholder="Ex:1600", min_value=1, max_value=3000, value=None, step=1)
+        nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord", placeholder="Ex:1800", min_value=1, max_value=6000, value=None, step=1)
+
         if st.sidebar.button("Modifier"):
-            fichier_modifie = modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name)
+            for dashboard_name in dashboards_a_modifier:
+                fichier_modifie = modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, [dashboard_name])
 
             # Télécharger le fichier modifié
             st.download_button(
