2c894afdc253cdc9e12a926581097541415057b7
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-11-30 17:54:00 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -2,11 +2,6 @@ import streamlit as st
 import xml.etree.ElementTree as ET
 from io import BytesIO
 
-def recuperer_noms_dashboards(xml_path):
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
-    return [dashboard.get("name") for dashboard in root.findall(".//dashboard")]
-
 def calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur, nouvelle_hauteur):
     nouveau_x = x / (100000 / maxwidth) * (100000 / nouvelle_largeur)
     nouveau_w = w / (100000 / maxwidth) * (100000 / nouvelle_largeur)
@@ -44,18 +39,17 @@ def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashb
 
 def main():
     st.title("Modification de Tableau de Bord")
-    # Sidebar
-    xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"])
-    st.write(ET.parse(xml_path))
-    if xml_path:
-        dashboards = recuperer_noms_dashboards(xml_path)
-        dashboard_a_modifier = st.sidebar.selectbox("Dashboards à modifier", dashboards)
-        st.write('You selected:', dashboard_a_modifier)
-        nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord", placeholder="Ex:1600", min_value=1, max_value=3000, value=None, step=1)
-        nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord", placeholder="Ex:1800", min_value=1, max_value=6000, value=None, step=1)
 
+    # Sidebar
+    dashboard_name = st.sidebar.text_input("Nom du Tableau de Bord", placeholder="Ex: Overview")
+    nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord",placeholder="Ex:1600",min_value=1, max_value=3000, value=None, step=1)
+    nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord",placeholder="Ex:1800",min_value=1, max_value=6000, value=None, step=1)
+    xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"])
+
+    if xml_path:
         if st.sidebar.button("Modifier"):
-            fichier_modifie = modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_a_modifier)
+            fichier_modifie = modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name)
+
             # Télécharger le fichier modifié
             st.download_button(
                 label="Télécharger le fichier modifié",
