528f524e02674fd152ae0d7f5d959547185b50b1
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-11-30 17:31:56 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -52,6 +52,7 @@ def main():
     if xml_path :
         dashboards = recuperer_noms_dashboards(xml_path)
         dashboard_a_modifier = st.sidebar.selectbox("Dashboards à modifier", dashboards)
+        st.write(dashboard_a_modifier)
         nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord", placeholder="Ex:1600", min_value=1, max_value=3000, value=None, step=1)
         nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord", placeholder="Ex:1800", min_value=1, max_value=6000, value=None, step=1)
 
