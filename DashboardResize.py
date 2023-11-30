42bf9c88db4097d5178f98c123c15e3d3549a3e2
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-11-30 17:47:06 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -2,6 +2,11 @@ import streamlit as st
 import xml.etree.ElementTree as ET
 from io import BytesIO
 
+def recuperer_noms_dashboards(xml_path):
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    return [dashboard.get("name") for dashboard in root.findall(".//dashboard")]
+
 def calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur, nouvelle_hauteur):
     nouveau_x = x / (100000 / maxwidth) * (100000 / nouvelle_largeur)
     nouveau_w = w / (100000 / maxwidth) * (100000 / nouvelle_largeur)
@@ -9,16 +14,11 @@ def calculer_nouvelles_valeurs(x, w, y, h, maxwidth, maxheight, nouvelle_largeur
     nouveau_h = h / (100000 / maxheight) * (100000 / nouvelle_hauteur)
     return int(nouveau_x), int(nouveau_w), int(nouveau_y), int(nouveau_h)
 
-def recuperer_noms_dashboards(xml_path):
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
-    return [dashboard.get("name") for dashboard in root.findall(".//dashboard")]
-
-def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboards_a_modifier):
+def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashboard_name):
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
-    for dashboard in root.findall(".//dashboard[@name='"+dashboards_a_modifier+"']"):
+    for dashboard in root.findall(".//dashboard[@name='"+dashboard_name+"']"):
         for size in dashboard.findall(".//size"):
             maxwidth = float(size.get("maxwidth", 1.0))
             size.set("maxwidth", str(nouvelle_largeur))
@@ -37,20 +37,20 @@ def modifier_tableau_de_bord(xml_path, nouvelle_largeur, nouvelle_hauteur, dashb
             zone.set("w", str(nouveau_w))
             zone.set("y", str(nouveau_y))
             zone.set("h", str(nouveau_h))
-
+            
     nouveau_nom_fichier = "nouveau_fichier.twb"
     tree.write(nouveau_nom_fichier)
     return nouveau_nom_fichier
 
 def main():
     st.title("Modification de Tableau de Bord")
-
     # Sidebar
     xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"])
     if xml_path :
         dashboards = recuperer_noms_dashboards(xml_path)
         dashboard_a_modifier = st.sidebar.selectbox("Dashboards à modifier", dashboards)
         st.write('You selected:', dashboard_a_modifier)
+        st.write(xml_path)
         nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord", placeholder="Ex:1600", min_value=1, max_value=3000, value=None, step=1)
         nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord", placeholder="Ex:1800", min_value=1, max_value=6000, value=None, step=1)
 
