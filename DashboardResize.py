b56b78545ecf561834eb382b4c08c500e0616697
Alexjuventiino <96688331+Alexjuventiino@users.noreply.github.com>
2023-12-01 08:48:20 +0100

    Update DashboardResize.py

--- DashboardResize.py
+++ DashboardResize.py
@@ -49,7 +49,7 @@ def main():
     dashboard_name = st.sidebar.text_input("Nom du Tableau de Bord", placeholder="Ex: Overview")
     nouvelle_largeur = st.sidebar.number_input("Nouvelle largeur du Tableau de Bord",placeholder="Ex:1600",min_value=1, max_value=3000, value=None, step=1)
     nouvelle_hauteur = st.sidebar.number_input("Nouvelle hauteur du Tableau de Bord",placeholder="Ex:1800",min_value=1, max_value=6000, value=None, step=1)
-    xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"])
+    xml_path = st.sidebar.file_uploader("Uploader le fichier .twb", type=["twb"], key="file_upload_widget")
     myfile = st.session_state.file_upload_widget
     dashboards = recuperer_noms_dashboards(myfile)
     st.write(ET.parse(myfile))
